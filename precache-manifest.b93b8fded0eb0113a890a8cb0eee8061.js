c423e096680ccdb7eb12b86375e909548436858a
harshdsdh <harsh.dsdh@gmail.com>
2020-08-11 01:24:59 -0400

    Updates

---
+++ precache-manifest.b93b8fded0eb0113a890a8cb0eee8061.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4484b2734bafdcb432cd4acf41ea3a5",
+    "url": "/eventUi/index.html"
+  },
+  {
+    "revision": "20112e8365c4f7f1c297",
+    "url": "/eventUi/static/css/main.e6c1f455.chunk.css"
+  },
+  {
+    "revision": "4c97148415cf5a670d20",
+    "url": "/eventUi/static/js/2.618eff3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/eventUi/static/js/2.618eff3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20112e8365c4f7f1c297",
+    "url": "/eventUi/static/js/main.2fb10e5a.chunk.js"
+  },
+  {
+    "revision": "0990b1499dbbee93645b",
+    "url": "/eventUi/static/js/runtime-main.6568e706.js"
+  }
+]);
